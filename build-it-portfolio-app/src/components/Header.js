69d8d27e12ba96366fc5af4045134a1d58188c03
Phil King <phil@beardedphil.com>
2020-02-20 09:38:52 -0600

    Nearly done with section 9. Committing current state so that I can run an update on React to remove deprecated lifecycle names.

---
+++ build-it-portfolio-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import {NavLink} from "react-router-dom";
+import React from "react";
+
+const Header = () => (
+    <header>
+        <h1>Build It - Portfolio</h1>
+        <NavLink to="/" activeClassName="is-active" exact={ true }>Home</NavLink>
+        <NavLink to="/portfolio" activeClassName="is-active">Portfolio</NavLink>
+        <NavLink to="/portfolio/:id" activeClassName="is-active">Portfolio</NavLink>
+        <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
+    </header>
+);
+
+export default Header;
