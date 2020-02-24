666fa375796dba1d838952a5ee5b533fe0b38ac4
Phil King <phil@beardedphil.com>
2020-02-24 09:09:29 -0600

    Removed node_modules from git.  End of section 11.

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses"
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+// store.subscribe(() => {
+//     const state = store.getState();
+//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//     console.log(visibleExpenses);
+// });
+
+store.dispatch(addExpense({
+    description: 'Water bill',
+    amount: 1200,
+    createdAt: 1000,
+}));
+
+store.dispatch(addExpense({
+    description: 'Gas bill',
+    amount: 9500,
+    createdAt: 8000,
+}));
+
+store.dispatch(addExpense({
+    description: 'Rent',
+    amount: 85000,
+    createdAt: 5000,
+}));
+
+// store.dispatch(setTextFilter('Water'));
+
+// setTimeout(() => {
+//     store.dispatch(setTextFilter());
+// }, 3000);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
