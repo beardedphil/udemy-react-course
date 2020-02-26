9b53327fdadace705f3478ef837b66d51f5bbf1a
Phil King <phil@beardedphil.com>
2020-02-25 18:41:36 -0600

    Lecture 122.  Trying again to resolve errors related to history/createBrowserHistory and history/createHashHistory, as well as warnings related to compoentWillReceiveProps and componentWillUpdate.

---
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import ExpenseForm from '../../components/ExpenseForm';
+
+
+test('should render ExpenseForm correctly', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseForm correctly with expense data', () => {
+    const wrapper = shallow(<ExpenseForm expense={ expenses[0] }/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render error for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+});
